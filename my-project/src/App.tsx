277109b5c5cdd5433e9e895b00bd4556c2fb55af
Lamassu15 <bazo.assyr@gmail.com>
2025-02-07 16:43:47 +0100

    React + Tailwind ? TS

---
+++ my-project/src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold text-center text-blue-500">Tailwind fungerar!</div>
+    </>
+  );
+}
+
+export default App;
